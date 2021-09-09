9cefeae394a743ccacaf092b80537d73ed348f3a
aidencuneo <aidencuneo@gmail.com>
2021-09-10 02:05:45 +1000

    Another quick commit

--- examples/gol.sh
+++ examples/gol.sh
@@ -3,20 +3,8 @@
 # Author: Aiden Blishen Cuneo
 # (Only this implementation, Game of Life was invented by John Conway)
 
-# Game board will be stored in the stack, and a dead cell will be 1, while a
-# living cell will be 2, and the end of the game board will be 0.
-
-5=w # Width (42) # aaaa2
+05=w # Width (42) # aaaa2
      # Height will be infinite
-
-# fun printf 1 *
-Fp
-    <=A # len
-    $A :
-        < P
-        $A -1+ =A $A
-    ;
-;
 
 # fun display
 Fd
@@ -36,11 +24,8 @@ Fd
     0aP # Trailing newline
 ;
 
-' :htdiW' 7 > Fp
+' :htdiW'
 $w p
 0aP
-
-0
-1]1]1]2]1]2]2]1]1]1]2]2]2]2]1]1]1]2]1]
 
 Fd
